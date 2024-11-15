7a0b5aaef2d9f744515cf89e87228ae40a05bfbb
red-jade <katagorikal@gmail.com>
2024-11-15 14:51:02 +0700

    Streamline build, roll version

--- deps.ex
+++ deps.ex
@@ -1,8 +1,7 @@
 [
-  {:exa_core,
-   [git: "https://github.com/red-jade/exa_core.git", tag: "v0.3.1", app: false]},
+  {:exa_core, [git: "https://github.com/red-jade/exa_core.git", tag: "v0.3.2"]},
   {:exa_space,
-   [git: "https://github.com/red-jade/exa_space.git", tag: "v0.3.1", app: false]},
+   [git: "https://github.com/red-jade/exa_space.git", tag: "v0.3.2"]},
   {:dialyxir, "~> 1.0", [only: [:dev, :test], runtime: false]},
   {:ex_doc, "~> 0.30", [only: [:dev, :test], runtime: false]}
 ]
--- mix.exs
+++ mix.exs
@@ -3,38 +3,9 @@ defmodule Exa.Std.MixProject do
 
   @lib :exa_std
   @name "Exa Std"
-  @ver "0.3.1"
-
-  # umbrella project
-  @exa {:exa,
-        git: "https://github.com/red-jade/exa.git",
-        branch: "main",
-        only: [:dev, :test],
-        runtime: false}
-
-  # dependency config code
-  @mix_util Path.join(["deps", "exa", "mix_util.ex"])
+  @ver "0.3.2"
 
   def project do
-    exa_deps =
-      cond do
-        System.fetch_env("EXA_BUILD") in [:error, {:ok, "rel"}] ->
-          # read auto-generated deps file
-          "deps.ex" |> Code.eval_file() |> elem(0)
-
-        File.regular?(@mix_util) ->
-          # generate deps using exa umbrella project
-          if not Code.loaded?(Exa.MixUtil) do
-            [{Exa.MixUtil, _}] = Code.compile_file(@mix_util)
-          end
-
-          Exa.MixUtil.exa_deps(@lib, exa_libs())
-
-        true ->
-          # bootstrap from exa umbrella project
-          [@exa]
-      end
-
     [
       app: @lib,
       name: @name,
@@ -42,7 +13,7 @@ defmodule Exa.Std.MixProject do
       elixir: "~> 1.17",
       erlc_options: [:verbose, :report_errors, :report_warnings, :export_all],
       start_permanent: Mix.env() == :prod,
-      deps: exa_deps ++ local_deps(),
+      deps: exa_deps() ++ local_deps(),
       docs: docs(),
       test_pattern: "*_test.exs",
       dialyzer: [flags: [:no_improper_lists]]
@@ -55,7 +26,7 @@ defmodule Exa.Std.MixProject do
     ]
   end
 
-  def docs do
+  defp docs do
     [
       main: "readme",
       output: "doc/api",
@@ -75,5 +46,55 @@ defmodule Exa.Std.MixProject do
 
   defp local_deps() do
     []
+  end
+
+  # ------------------
+  # EXA umbrella build 
+  # ------------------
+
+  # umbrella project
+  @exa {:exa,
+        git: "https://github.com/red-jade/exa.git",
+        branch: "main",
+        only: [:dev, :test],
+        runtime: false}
+
+  # dependency config code
+  @mix_util Path.join(["deps", "exa", "mix_util.ex"])
+
+  defp exa_deps() do
+    cond do
+       arg_scope() == :rel -> 
+        # read auto-generated deps file
+        "deps.ex" |> Code.eval_file() |> elem(0) 
+
+      File.regular?(@mix_util) ->
+        # generate deps using exa umbrella project
+        if not Code.loaded?(Exa.MixUtil) do
+          [{Exa.MixUtil, _}] = Code.compile_file(@mix_util)
+        end
+
+        Exa.MixUtil.exa_deps(@lib, exa_libs())
+      true ->
+        # bootstrap from exa umbrella project
+        [@exa]
+      end
+  end
+
+  # get scope from env var or cmd line
+  # return atom, default to :rel
+  defp arg_scope() do
+    default =
+      case System.fetch_env("EXA_BUILD") do
+        :error -> "rel"
+        {:ok, mix_build} -> mix_build
+      end
+
+    System.argv()
+    |> tl()
+    |> OptionParser.parse(strict: [build: :string])
+    |> elem(0)
+    |> Keyword.get(:build, default)
+    |> String.to_atom()
   end
 end
